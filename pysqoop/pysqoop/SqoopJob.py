ede2ee1896d15f1e83d83e439cd5b47409cdb274
marco gallegos <ma_galeza@hotmail.com>
2020-03-05 10:55:33 -0600

    [imp] sqoopjob: basic job management
    job management added with basic functions, only implemented job list and
    set priority

--- pysqoop/pysqoop/SqoopJob.py
+++ pysqoop/pysqoop/SqoopJob.py
@@ -2,7 +2,7 @@
 '''
 Author: Marco A. Gallegos
 Date: 2020/02/03
-Description: manage scoop jobs, we going to consider list sqoop jobs,
+Description: manage sqoop jobs, we going to consider list sqoop jobs,
 store sqoop jobs, delete sqoop jobs, cancel running sqoop jobs
 
 https://docs.python.org/3.7/library/subprocess.html
@@ -16,10 +16,12 @@ class SqoopJob(object):
     def __init__(
             self, job_id=None
     ):
+        self.priority_options=[]
         self._properties = collections.OrderedDict()
     
     @staticmethod
     def list_runing_jobs():
+        '''List queued jobs if you are using ambari consider visit http://youambariip:8088'''
         command=["mapred","job","-list"]
         try:
             process = subprocess.run(
@@ -29,6 +31,23 @@ class SqoopJob(object):
             print(process.stdout)
         except Exception as e:
             print(e)
+            
+    def set_priority(self, priority:str)->bool:
+        '''change priority of a runing job VERY_HIGH HIGH NORMAL LOW VERY_LOW DEFAULT'''
+        command = "mapred job -set-priority {priority}"
+        if priority in priority_options:
+            try:
+                process = subprocess.run(
+                    command, stdout=subprocess.PIPE,
+                    encoding="utf-8"
+                    )
+                print(process.stdout)
+                return True
+            except Exception as e:
+                print(e)
+                return False
+        else:
+            return False
 
 if __name__ == "__main__":
     SqoopJob.list_runing_jobs()
